69184ea80427f53bfbcace535e12bcce24d265df
neet-no1 <no1.neet.professional@gmail.com>
2021-03-20 15:17:03 +0900

    モックAPIのバグ修正

--- src/scripts/mock/api_mock.js
+++ src/scripts/mock/api_mock.js
@@ -21,13 +21,12 @@ import menus_and_articles_success from "./data/menus_and_articles_success.json";
 
 export default class ApiMock {
 
-    POST = 'post';
-    GET = 'get';
-
     constructor() {
         console.log('ApiMock constructor');
         console.log($);
         this.load = this.load.bind(this);
+        this.POST = 'post';
+        this.GET = 'get';
     }
 
     load() {
@@ -35,259 +34,259 @@ export default class ApiMock {
         $.mockjax(new AjaxMock().success(
             URL.POST_LOGIN,
             basic_success,
-            POST
+            this.POST
         ));
 
         // お知らせ情報取得
         $.mockjax(new AjaxMock().success(
             URL.GET_NOTICE,
             notice_success,
-            GET
+            this.GET
         ));
 
         // ログイン状態取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_AUTH,
             account_auth_success,
-            GET
+            this.GET
         ));
 
         // アカウント登録
         $.mockjax(new AjaxMock().success(
             URL.POST_ACCOUNT_REGIST,
             basic_success,
-            POST
+            this.POST
         ));
 
         // メールアドレス有効性確認
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_EMAIL_CONFIRM,
             basic_success,
-            GET
+            this.GET
         ));
 
         // パスワードリセット
         $.mockjax(new AjaxMock().success(
             URL.POST_PASSWORD_RESET,
             basic_success,
-            POST
+            this.POST
         ));
 
         // アカウント情報取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_INFO,
             account_success,
-            GET
+            this.GET
         ));
 
         // アカウント情報更新
         $.mockjax(new AjaxMock().success(
             URL.POST_ACCOUNT_INFO_UPDATE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // パスワード更新
         $.mockjax(new AjaxMock().success(
             URL.POST_ACCOUNT_PASSWORD_UPDATE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // ユーザをフォローする
         $.mockjax(new AjaxMock().success(
             URL.POST_FOLLOW,
             basic_success,
-            POST
+            this.POST
         ));
 
         // フォロー・フォロワー取得
         $.mockjax(new AjaxMock().success(
             URL.GET_FOLLOW_LIST,
             followers_success,
-            GET
+            this.GET
         ));
 
         // おすすめ情報表示パラメタを取得
         $.mockjax(new AjaxMock().success(
             URL.GET_RECOMMEND_META,
             recommend_meta_success,
-            GET
+            this.GET
         ));
 
         // 新着献立取得
         $.mockjax(new AjaxMock().success(
             URL.GET_MENU_NEWARRIVAL,
             menus_success,
-            GET
+            this.GET
         ));
 
         // 人気献立取得
         $.mockjax(new AjaxMock().success(
             URL.GET_MENU_POPULAR,
             menus_success,
-            GET
+            this.GET
         ));
 
         // 投稿献立一覧取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_POSTED_MENUS,
             menus_success,
-            GET
+            this.GET
         ));
 
         // 献立検索
         $.mockjax(new AjaxMock().success(
             URL.GET_SEARCH_MENUS,
             menus_success,
-            GET
+            this.GET
         ));
 
         // 献立カテゴリ取得
         $.mockjax(new AjaxMock().success(
             URL.GET_MENU_CATEGORIES,
             menu_categories_success,
-            GET
+            this.GET
         ));
 
         // 献立投稿
         $.mockjax(new AjaxMock().success(
             URL.POST_MENU,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 献立内容取得
         $.mockjax(new AjaxMock().success(
             URL.GET_MENU,
             menu_data_success,
-            GET
+            this.GET
         ));
 
         // 献立削除
         $.mockjax(new AjaxMock().success(
             URL.POST_MENU_DELETE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 献立お気に入り追加・解除
         $.mockjax(new AjaxMock().success(
             URL.POST_MENU_FAVORITE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 新着記事取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ARTICLE_NEWARRIVAL,
             articles_success,
-            GET
+            this.GET
         ));
 
         // 投稿記事一覧取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_POSTED_ARTICLES,
             articles_success,
-            GET
+            this.GET
         ));
 
         // 記事検索
         $.mockjax(new AjaxMock().success(
             URL.GET_SEARCH_ARTICLES,
             articles_success,
-            GET
+            this.GET
         ));
 
         // 記事投稿
         $.mockjax(new AjaxMock().success(
             URL.POST_ARTICLE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 記事内容取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ARTICLE,
             article_data_success,
-            GET
+            this.GET
         ));
 
         // 記事削除
         $.mockjax(new AjaxMock().success(
             URL.POST_ARTICLE_DELTE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 記事お気に入り追加・解除
         $.mockjax(new AjaxMock().success(
             URL.POST_ARTICLE_FAVORITE,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 質問検索
         $.mockjax(new AjaxMock().success(
             URL.GET_SEARCH_QUESTIONS,
             questions_success,
-            GET
+            this.GET
         ));
 
         // 質問カテゴリ取得
         $.mockjax(new AjaxMock().success(
             URL.GET_QUESTION_CATEGORIES,
             question_categories_success,
-            GET
+            this.GET
         ));
 
         // 質問投稿
         $.mockjax(new AjaxMock().success(
             URL.POST_QUESTION,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 質問内容取得
         $.mockjax(new AjaxMock().success(
             URL.GET_QUESTION,
             question_data_success,
-            GET
+            this.GET
         ));
 
         // 回答投稿
         $.mockjax(new AjaxMock().success(
             URL.POST_QUESTION_ANSWER,
             basic_success,
-            POST
+            this.POST
         ));
 
         // 回答一覧取得
         $.mockjax(new AjaxMock().success(
             URL.GET_QUESTION_ANSWERS,
             answers_success,
-            GET
+            this.GET
         ));
 
         // ベストアンサー取得
         $.mockjax(new AjaxMock().success(
             URL.GET_QUESTION_BESTANSWER,
             best_answer_success,
-            GET
+            this.GET
         ));
 
         // ベストアンサー決定
         $.mockjax(new AjaxMock().success(
             URL.POST_QUESTION_BESTANSWER,
             basic_success,
-            POST
+            this.POST
         ));
 
         // お気に入り一覧を取得
         $.mockjax(new AjaxMock().success(
             URL.GET_ACCOUNT_FAVORITES,
             menus_and_articles_success,
-            GET
+            this.GET
         ));
     }
 }
