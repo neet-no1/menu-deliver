ab788d2786c7610095eb1370c9cc5910d96c416c
neet-no1 <no1.neet.professional@gmail.com>
2021-03-20 15:48:38 +0900

    APIからデータ取得対応(ログインページ)

--- src/scripts/Login.js
+++ src/scripts/Login.js
@@ -3,36 +3,26 @@ import ApiUtils from '../scripts/api_utils'
 
 export default {
     data() {
-        return {login: null}
+        return {
+            email: '',
+            password: ''
+        }
     },
     methods: {
         api_login: function(event) {
-            console.log('login URL:' + URL.POST_LOGIN);
-            let response = new ApiUtils().postAccess(URL.POST_LOGIN, {'user_id': 'user', 'user_password': 'password'}, (response) => {
-                // ???X?|???X??g???????L?????B
-                console.log('response: ');
-                console.log(response);
-            });
-        },
-        api_test: function(event) {
-            console.log('???i?? URL:' + URL.GET_NEW_ARRIVAL_PRODUCTS);
-            let response = new ApiUtils().getAccess(URL.GET_NEW_ARRIVAL_PRODUCTS, {}, (response) => {
-                // ???X?|???X??g???????L?????B
-                console.log('response: ');
-                console.log(response);
+            new ApiUtils().postAccess(URL.POST_LOGIN,
+                {
+                    'email': this.email,
+                    'password': this.password
+                },
+                (response) => {
+                if(response.code == 0) {
+                    // トップページへ遷移
+                    window.location.href = '/';
+                } else {
+                    alert('認証に失敗しました。再度お試しください。')
+                }
             });
         }
-    },
-    props: {
-        attrs: [
-            {
-                key: 'today',
-                highlight: {
-                  backgroundColor: '#ff8080',
-                },
-                dates: new Date()
-            }
-        ]
-    },
-
+    }
 }
