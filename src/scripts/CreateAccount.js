3d6acc3fa9ae2f74b949c5f89390d2c56bf00c05
neet-no1 <no1.neet.professional@gmail.com>
2021-03-22 18:30:00 +0900

    APIからデータ取得対応(アカウント登録)

--- src/scripts/CreateAccount.js
+++ src/scripts/CreateAccount.js
@@ -1,5 +1,37 @@
 import Vue from 'vue'
 
+import * as URL from '../common/api_url'
+import ApiUtils from '../scripts/api_utils'
+
 export default {
-    name: 'CreateAccount'
+    name: 'CreateAccount',
+    data() {
+        return {
+            email: '',
+            password: '',
+            password_confirm: '',
+        }
+    },
+    methods: {
+        create_account() {
+            new ApiUtils().postAccess(
+                URL.POST_ACCOUNT_REGIST,
+                {
+                    "email": this.email,
+                    "password": this.password,
+                    "passwordConfirm": this.password_confirm
+                },
+                (response) => {
+                    if (response.code == 0) {
+                        // アカウント登録が完了したら、仮完了画面へ遷移
+                        window.location.href = '/create/account/predone';
+                    } else {
+                        alert('エラーが発生しました。')
+                        console.log('パスワードリセットエラー')
+                        console.log(response)
+                    }
+                }
+            )
+        }
+    },
 }
