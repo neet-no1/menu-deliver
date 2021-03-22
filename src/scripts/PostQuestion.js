57cc6919a6f5f6e0103ac14d269e3ad03ab35802
neet-no1 <no1.neet.professional@gmail.com>
2021-03-23 00:46:38 +0900

    APIからデータ取得対応(質問投稿)

--- src/scripts/PostQuestion.js
+++ src/scripts/PostQuestion.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 
+import * as URL from '../common/api_url'
+import ApiUtils from '../scripts/api_utils'
+import CommonUtils from '../scripts/common_utils'
+
 import PageHeader from '../pages/module/PageHeader.vue'
 import PageFooter from '../pages/module/PageFooter.vue'
 
@@ -11,20 +15,41 @@ export default {
     },
     data() {
         return {
-            quill: null,
-            user_icon: {
-                'background-image': 'url("/public/img/user_1.jpg")'
-            },
-            menu_imgs_index: 0,
-            menu_imgs: [
-                {
-                    input_image: null,
-                    uploadImageUrl: '',
-                }
-            ],
+            // ユーザ名
+            user_name: '',
+
+            // ユーザアイコン
+            user_icon: {},
+
+            // 画像情報
+            input_image: null,
+
+            // 画像パス
+            uploadImageUrl: '',
+
+            // 質問内容
+            contents: '',
         }
     },
     methods: {
+        get_account() {
+            new ApiUtils().getAccess(
+                URL.GET_ACCOUNT_INFO,
+                {},
+                (response) => {
+                    if (response.code == 0) {
+                        this.user_name = response.info.name
+                        this.user_icon = {
+                            'background-image': 'url(' + response.info.imgPath + ')'
+                        }
+                    } else {
+                        alert('エラーが発生しました。')
+                        console.log('アカウント情報取得エラー')
+                        console.log(response)
+                    }
+                }
+            )
+        },
         onImagePicked(file) {
             let i = this.menu_imgs_index
             if (file !== undefined && file !== null) {
@@ -34,14 +59,35 @@ export default {
                 const fr = new FileReader()
                 fr.readAsDataURL(file)
                 fr.addEventListener('load', () => {
-                    this.menu_imgs[i].uploadImageUrl = fr.result
+                    this.uploadImageUrl = fr.result
                 })
             } else {
-                this.menu_imgs[i].uploadImageUrl = ''
+                this.uploadImageUrl = ''
             }
         },
-        selectImage(index) {
-            this.menu_imgs_index = index
+        post_question() {
+            let formData = new FormData()
+            formData.append("contents", this.contents)
+            formData.append("file", this.input_image)
+
+            new ApiUtils().formDataAccess(
+                URL.POST_QUESTION,
+                formData,
+                (response) => {
+                    if (response.code == 0) {
+                        // 成功したら、質問一覧ページへ遷移する
+                        window.location.href = '/question'
+                    } else {
+                        alert('エラーが発生しました。')
+                        console.log('質問投稿エラー')
+                        console.log(response)
+                    }
+                }
+            )
         },
+    },
+    mounted() {
+        new CommonUtils().isAuthToLogin()
+        this.get_account()
     }
 }
