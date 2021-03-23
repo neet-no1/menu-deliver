7137bbd62fc5e1e36e112e110120d7fbbcdb03c8
neet-no1 <no1.neet.professional@gmail.com>
2021-03-23 20:04:22 +0900

    APIからデータ取得対応(ヘッダー部分)

--- src/scripts/PageHeader.js
+++ src/scripts/PageHeader.js
@@ -1,14 +1,55 @@
 import Vue from 'vue'
+import * as URL from '../common/api_url'
+import ApiUtils from '../scripts/api_utils'
 
 export default {
     name: 'PageHeader',
     data() {
         return {
+            // モーダルを開く
             select_post_type: false,
-            isLogin: false
+            
+            // ログインしているか
+            isLogin: false,
+
+            // ユーザアイコン
+            user_icon: '',
         }
     },
     methods: {
+        get_is_auth() {
+            new ApiUtils().getAccess(
+                URL.GET_ACCOUNT_AUTH,
+                {},
+                (response) => {
+                    if(response.code == 0) {
+                        this.isLogin = response.info
+
+                        // アカウント情報取得
+                        this.get_account_info()
+                    } else {
+                        alert('エラーが発生しました。')
+                        console.log('ログイン状態取得エラー')
+                        console.log(response)
+                    }
+                }
+            )
+        },
+        get_account_info() {
+            new ApiUtils().getAccess(
+                URL.GET_ACCOUNT_INFO,
+                {},
+                (response) => {
+                    if(response.code == 0) {
+                        this.user_icon = response.info.imgPath
+                    } else {
+                        alert('エラーが発生しました。')
+                        console.log('アカウント情報取得エラー')
+                        console.log(response)
+                    }
+                }
+            )
+        },
         post_menu_or_article: function() {
             this.select_post_type = this.select_post_type == false
         },
@@ -21,5 +62,8 @@ export default {
                 event.stopPropagation()
             }
         }
+    },
+    mounted() {
+        this.get_is_auth()
     }
 }
