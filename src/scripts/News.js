543164a568ec67b7c5c249bb9a000d9ff767a4d3
neet-no1 <no1.neet.professional@gmail.com>
2021-03-23 20:20:09 +0900

    APIからデータ取得対応(お知らせ部分)

--- src/scripts/News.js
+++ src/scripts/News.js
@@ -1,5 +1,33 @@
 import Vue from 'vue'
+import * as URL from '../common/api_url'
+import ApiUtils from '../scripts/api_utils'
 
 export default {
-    name: 'News'
+    name: 'News',
+    data() {
+        return {
+            // お知らせ情報
+            news_list: [],
+        }
+    },
+    methods: {
+        get_news() {
+            new ApiUtils().getAccess(
+                URL.GET_NOTICE,
+                {},
+                (response) => {
+                    if (response.code == 0) {
+                        this.news_list = response.info
+                      } else {
+                        alert('エラーが発生しました。')
+                        console.log('お知らせ情報取得エラー')
+                        console.log(response)
+                      }
+                }
+            )
+        }
+    },
+    mounted() {
+        this.get_news()
+    }
 }
